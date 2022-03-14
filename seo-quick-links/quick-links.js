9098db0de6715727d60c175c31d7a5c190d870d3
seowner <41257893+seowner@users.noreply.github.com>
2022-03-14 01:15:47 -0400

    Update quick-links.js

--- seo-quick-links/quick-links.js
+++ seo-quick-links/quick-links.js
@@ -7,11 +7,11 @@ const getHostname = (url) => {
   return new URL(url).hostname;
 }
 
-url = url.replace("www.", ""); 
-
-    document.getElementById("maj_link").innerHTML = '<a href="https://majestic.com/reports/site-explorer?q='+getHostname(url)+'&oq='+getHostname(url)+'&IndexDataSource=F" target="_blank">Majestic SEO</a>';
+    document.getElementById("google_cache_link").innerHTML = '<a href="https://webcache.googleusercontent.com/search?q=cache:'+url+'" target="_blank">Google Cache</a>';
+    url = url.replace("www.", ""); 
+    document.getElementById("maj_link").innerHTML = '<a href="https://majestic.com/reports/site-explorer?q='+getHostname(url)+'&oq='+getHostname(url)+'&IndexDataSource=F" target="_blank">View Majestic SEO Report</a>';
     document.getElementById("google_link").innerHTML = '<a href="https://www.google.com/search?q=site:'+getHostname(url)+'" target="_blank">Check Indexed Pages</a>';
+    document.getElementById("whois_link").innerHTML = '<a href="https://whois.domaintools.com/'+getHostname(url)+'" target="_blank">Check WHOIS</a>';
     document.getElementById("semrush_link").innerHTML = '<a href="https://www.semrush.com/analytics/overview/?q='+getHostname(url)+'&searchType=domain" target="_blank">SEMRush</a>';
-    document.getElementById("ahrefs_link").innerHTML = '<a href="https://ahrefs.com/site-explorer/overview/v2/subdomains/live?target='+getHostname(url)+'" target="_blank">Ahrefs</a>';
 
 });
